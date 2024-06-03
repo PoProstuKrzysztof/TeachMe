bfceaf43e7c436a1b5177f17892c02d9f5bc687e
Chris <krzysiek.palonek@gmail.com>
2024-06-03 16:31:27 +0200

    Notifications

---
+++ app/src/main/java/com/example/teachme/models/NotificationUtils.kt
@@ -0,0 +1,45 @@
+package com.example.teachme.models
+
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.teachme.R
+
+object NotificationUtils {
+
+    private const val CHANNEL_ID = "LESSON_CHANNEL"
+    private const val CHANNEL_NAME = "New Lesson Notifications"
+    private const val CHANNEL_DESCRIPTION = "Notifications for new lessons"
+
+    fun createNotificationChannel(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                description = CHANNEL_DESCRIPTION
+            }
+            val notificationManager: NotificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    fun sendNewLessonNotification(context: Context) {
+        val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Nowa lekcja dodana!")
+            .setContentText("Sprawdź nową lekcję dodaną do aplikacji.")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(context)) {
+            notify(1001, builder.build())
+        }
+    }
+}
